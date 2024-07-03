e31f5ea1ca093091ceeabffdafed5f57a8b3e1f1
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-07-04 00:33:34 +0800

    perf: skip resolve builtinModules

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { dirname } from 'node:path'
+import { isBuiltin } from 'node:module'
 import type { NapiResolveOptions } from 'oxc-resolver'
 import { ResolverFactory } from 'oxc-resolver'
 import { normalizeOptions } from './nomalizeOptions'
 
 let resolver: ResolverFactory | undefined
-export function resolve(source: string, file: string, options: NapiResolveOptions | null = {}): { found: boolean, path?: string } {
+export function resolve(source: string, file: string, options: NapiResolveOptions | null = {}): { found: boolean, path: string | null | undefined } {
+  if (isBuiltin(source))
+    return { found: true, path: null }
+
   if (!resolver) {
     options = normalizeOptions(options)
     resolver = new ResolverFactory(options)
