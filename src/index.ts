d4fc2360c88ac520d4ea89592a122a1692792385
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-07-04 23:49:29 +0800

    chore: correct filename

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { dirname } from 'node:path'
 import { isBuiltin } from 'node:module'
 import type { NapiResolveOptions } from 'oxc-resolver'
 import { ResolverFactory } from 'oxc-resolver'
-import { normalizeOptions } from './nomalizeOptions'
+import { normalizeOptions } from './normalizeOptions'
 
 let resolver: ResolverFactory | undefined
 export function resolve(source: string, file: string, options: NapiResolveOptions | null = {}): { found: boolean, path: string | null | undefined } {
