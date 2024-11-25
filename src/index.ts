cc6a4c9d46e82afa8abd0b8cca48c120edd14e5e
Vida Xie <38204901+9romise@users.noreply.github.com>
2024-11-25 22:12:53 +0800

    fix: ensure resolver reinitializes when options change

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@ import { isBuiltin } from 'node:module'
 import { dirname } from 'node:path'
 import { ResolverFactory } from 'oxc-resolver'
 import { normalizeOptions } from './normalizeOptions'
+import { hashObject } from './utils'
 
+let cacheOptionsHash: string | undefined
 let resolver: ResolverFactory | undefined
-export function resolve(source: string, file: string, options: NapiResolveOptions | null = {}): { found: boolean, path: string | null | undefined } {
+export function resolve(source: string, file: string, options?: NapiResolveOptions | null): { found: boolean, path: string | null | undefined } {
   if (isBuiltin(source))
     return { found: true, path: null }
 
-  if (!resolver) {
+  options ??= {}
+  const optionsHash = hashObject(options)
+
+  if (!resolver || cacheOptionsHash !== optionsHash) {
     options = normalizeOptions(options)
     resolver = new ResolverFactory(options)
+    cacheOptionsHash = optionsHash
   }
 
   // https://github.com/oxc-project/oxc-resolver/blob/main/npm/README.md#api
